25018e02b2ae5a602db1636165630c172f1ebbd9
SabirAliyev <sabir.aliyev.itdep@gmail.com>
2022-12-25 22:05:04 +0400

    create DAO GET for Person model

---
+++ src/main/java/com/sabirali/springcource/model/Person.java
@@ -0,0 +1,29 @@
+package com.sabirali.springcource.model;
+
+import java.util.PrimitiveIterator;
+
+public class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
