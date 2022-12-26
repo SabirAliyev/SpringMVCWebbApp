ef00144a09d3f5c31ece52c78280b4fcd6ac87f4
SabirAliyev <sabir.aliyev.itdep@gmail.com>
2022-12-26 08:17:04 +0400

    implement adding new person to the list, using POST method

--- src/main/java/com/sabirali/springcource/controllers/PeopleController.java
+++ src/main/java/com/sabirali/springcource/controllers/PeopleController.java
@@ -1,12 +1,11 @@
 package com.sabirali.springcource.controllers;
 
 import com.sabirali.springcource.dao.PersonDAO;
+import com.sabirali.springcource.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -30,4 +29,36 @@ public class PeopleController {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
+
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+
+        return "people/new";
+    }
+
+    //// or we can use @ModelAttribute also
+//    @GetMapping("/new")
+//    public String newPerson(@ModelAttribute("person") Person person) {
+//        return "people/new";
+//    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
